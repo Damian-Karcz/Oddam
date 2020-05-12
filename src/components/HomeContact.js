d903ee24b42c17a82538114d2970f3defa3060d8
Damian Karcz <damian.karcz95@tlen.pl>
2020-05-12 15:39:48 +0200

    Sending a form (APi to check - not working)

--- src/components/HomeContact.js
+++ src/components/HomeContact.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 import decoration from "../assets/Decoration.svg";
 import facebookImg from "../assets/Facebook.svg"
 import instagramImg from "../assets/Instagram.svg"
 
 export default function HomeContact() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
 
+    const handleChangeName = (e) => {
+        setName(e.target.value)
+    }
+    const handleChangeEmail = (e) => {
+        setEmail(e.target.value)
+    }
+    const handleChangeMessage = (e) => {
+        setMessage(e.target.value)
+    }
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        const form = {
+            Name: name,
+            Email: email,
+            Message: message
+        }
+        const Api = "https://fer-api.coderslab.pl/v1/portfolio/contact";
+        fetch(`${Api}`, {
+            method: "POST",
+            body: JSON.stringify(form),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+    }
 
     return (
         <>
@@ -16,15 +45,15 @@ export default function HomeContact() {
                 <div className="formDiv">
                     <h1>Skontaktuj się z nami</h1>
                     <img src={decoration} alt="Decoration Border"/>
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <label> Wpisz swoje imię
-                            <input placeholder="Krzysztof"/>
+                            <input type="text" placeholder="Krzysztof" onChange={handleChangeName} value={name}/>
                         </label>
                         <label> Wpisz swój email
-                            <input type="email" placeholder="abc@xyz.pl"/>
+                            <input type="email" placeholder="abc@xyz.pl" onChange={handleChangeEmail} value={email}/>
                         </label>
                         <label> Wpisz swoją wiadomość
-                            <textarea placeholder="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."/>
+                            <textarea onChange={handleChangeMessage} value={message} placeholder="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."/>
                         </label>
                         <input className="buttonsHeader" type="submit" value="Wyślij"></input>
                     </form>
